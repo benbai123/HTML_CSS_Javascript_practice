e75169838815828aef25c5ec06f8f686001588ad
benbai123 <benbai123@gmail.com>
2014-11-24 11:50:44 +0800

    add tmp

---
+++ tmp/test.js
@@ -0,0 +1,20 @@
+(function () {
+	var processClick = function (e) {
+		var dom = e.target,
+			$dom = $(dom);
+		console.log($dom.attr('tagName'));
+		if ($dom.attr('type') == 'checkbox'
+			|| ($dom.prop('tagName').toLowerCase() == 'label'
+				&& (dom = $dom.find('input')[0])
+				&& $(dom).attr('type') == 'checkbox')) {
+			processCheckboxClick();
+		}
+		
+		console.log($(dom).attr('tagName'));
+		
+	};
+	var processCheckboxClick = function () {
+		alert('process checkbox click');
+	}
+	$(document.body).on(click, processClick);
+})();
