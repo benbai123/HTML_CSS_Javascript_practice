f03a2f035a0c2886afa4674e6c3793b3f66c79f0
Ben <benbai123@gmail.com>
2016-09-23 17:26:13 +0800

    Update uccustomize.js

--- tmp/uccustomize.js
+++ tmp/uccustomize.js
@@ -126,32 +126,138 @@
 					wgt._addParam(param);
 				});
 		},
+		_processItemClick: function (e, $item) {
+			var wgt = this;
+			// 點擊時
+			e.preventDefault();
+			// 找到選項下 checkbox
+			var $cbx = $item.find('.custom-checkbox');
+			// 改變勾選狀態
+			if ($cbx.hasClass('custom-checkbox-checked')) {
+				$cbx.removeClass('custom-checkbox-checked')
+					.removeClass('show');
+			} else {
+				$cbx.addClass('custom-checkbox-checked')
+					.addClass('show');
+				// 若為選取時, 移除第二選取區中的項目
+				var lb = $item.find('.label').text(),
+					title = $item.closest('.group-box')
+						.find('.group-box-header .group-box-title').text();
+				$('.second-level-cond-block')
+					.find('[data-label="'+lb+'"][data-title="'+title+'"]').remove();
+			}
+			// 重生右側上方選項
+			set_right_condistion();
+			wgt._initConditions();
+			// 若點擊時沒有按著 Ctrl 則刷新頁面
+			if (!e.ctrlKey) {
+				wgt.ajaxLoad();
+			}
+		},
+		// 將項目存到第二區
+		// $conds: 傳入的上方選項
+		_addToSecond: function ($conds) {
+			// $scondBlock: 第二區
+			var $scondBlock = $('.second-level-cond-block'),
+				wgt = this;
+			// 若無第二區就先生成
+			if (!$scondBlock[0]) {
+				// $cnt: 第二區內部內容
+				// $clear: 第二區整區清除鈕
+				var $cnt = $('<div class="cnt"></div>'),
+					$clear = $('<span>全部清除</span>');
+				$scondBlock = $('<div class="second-level-cond-block"></div>');
+				// 調整 style
+				$scondBlock.css({
+					'position': 'relative', 'height': '25px', 'left': '5px'
+				});
+				$cnt.css({
+					'position': 'absolute', 'left': '0', 'top': '0', 'z-index': '999999',
+					'width': '220px', 'height': '30px', 'overflow': 'hidden',
+					'background-color': '#CCCCCC', 'white-space': 'normal'
+				});
+				$clear.css({
+					'color': '#DD406F', 'cursor': 'pointer', 'font-size': '18px'
+				});
+				// 移入時展開, 移出時收起
+				$cnt.on('mouseenter', function () {
+					$cnt.css('height', 'auto');
+				}).on('mouseleave', function () {
+					$cnt.css('height', '30px');
+				});
+				// 點擊全部清除時移除第二區
+				$clear.on('click', function () {
+					$scondBlock.remove();
+				});
+				// 將第二區加到頁面
+				$cnt.append($clear);
+				$scondBlock.append($cnt);
+				$('.product-filter-list').before($scondBlock);
+			}
+			// 每一個上方選項
+			$conds.each(function () {
+				// 由選項所帶的 url 找到對應左方選項
+				// 取得選項文字及區塊 title
+				// $scond: 第二區選項 (待生成)
+				// $cross: 選項的移除鈕
+				var url = $(this).attr("s_href"),
+					$opt = $('.group-box-body a[s_href="'+url+'"]'),
+					lb = $opt.find('.label').text(),
+					title = $opt.closest('.group-box')
+						.find('.group-box-header .group-box-title').text(),
+					$scond,
+					$cross = $('<span>&times;</span>');
+					// 若已有該選項則不再添加
+					if ($scondBlock.find('[data-label="'+lb+'"][data-title="'+title+'"]')[0]) return;
+					// 生成選項內容
+					$scond = $('<div data-label="'+lb+'" data-title="'+title+'" title="'+title+'--'+lb+'">'+lb+'</div>');
+					// 調整 style
+					$scond.css({
+						'display': 'inline-block', 'font-size': '18px', 'color': '#888888',
+						'padding': '5px 10px', 'cursor': 'pointer'
+					});
+					$cross.css({
+						'color': '#DD406F'
+					});
+					// 點擊時
+					// 若點在 $cross 上則移除選項
+					// 若點在其它部份則重新選取選項
+					$scond.on('click', function (e) {
+						if (e.target == $(this).find('span')[0]) {
+							$(this).remove();
+						} else {
+							// 從 $scond 上取出 title 及 label
+							var slb = $(this).attr('data-label'),
+								stitle = $(this).attr('data-title'),
+								$item;
+							$(this).remove();
+							// 由 title 及 label 找到對應的左方選項
+							$item = $('.group-box').filter(function () {
+								return $(this).find('.group-box-header .group-box-title').text() == stitle;
+							}).find('a')
+							.filter(function () {
+								return $(this).find('.label').text() == slb;
+							});
+							// 若為已選取狀態則略過
+							if ($item.find('.custom-checkbox-checked')[0]) return;
+							// 否則當選項點擊處理
+							wgt._processItemClick(e, $item);
+						}
+					});
+					// 將項目內容加到頁面
+					$scond.append($cross);
+					$scondBlock.find('.cnt').append($scond);
+			});
+		},
 		// 初始化左方選項
 		_initSelections: function () {
 			var wgt = this;
 			// 所有選項
 			// 移除原本綁定
+			// 重新綁定點擊事件
 			$('.group-box-body a').off()
-				.click(function(e){
-					// 點擊時
-					e.preventDefault();
-					// 找到選項下 checkbox
-					var $cbx = $(this).find('.custom-checkbox');
-					// 改變勾選狀態
-					if ($cbx.hasClass('custom-checkbox-checked')) {
-						$cbx.removeClass('custom-checkbox-checked')
-							.removeClass('show');
-					} else {
-						$cbx.addClass('custom-checkbox-checked')
-							.addClass('show');
-					}
-					// 重生右側上方選項
-					set_right_condistion();
-					wgt._initConditions();
-					// 若點擊時沒有按著 Ctrl 則刷新頁面
-					if (!e.ctrlKey) {
-						wgt.ajaxLoad();
-					}
+				.click(function(e) {
+					wgt._processItemClick(e, $(this));
 				});
 		},
 		// 初始化上方條件
@@ -166,9 +272,11 @@
 						$opt = $('.group-box-body a[s_href="'+url+'"]'),
 						$cbx = $opt.find('.custom-checkbox');
 					// 被點擊時, 移除左方選項的選取狀態
+					// 將項目轉存到第二層
 					// 也移除本身的 dom 元素
 					$cbx.removeClass('custom-checkbox-checked')
 							.removeClass('show');
+					wgt._addToSecond($(this));
 					$(this).remove();
 					// 若點擊時沒有按著 Ctrl 鍵
 					// 則更新頁面
@@ -181,6 +289,8 @@
 			$('.clear').off().
 				click(function(e){
 				e.preventDefault();
+				// 將項目轉存到第二層
+				wgt._addToSecond($('.condition'));
 				// 移除所有上方選項
 				$('.condition').remove();
 				// 清除所有選取狀態
@@ -215,6 +325,9 @@
 			n += p.join('&');
 			// 存下本次 URL
 			this._previousURL = n;
+			if (window.history && window.history.pushState) {
+				window.history.pushState(n, document.title, n);
+			}
 			$.ajax({
 					url: n,
 					type: "GET",
@@ -234,33 +347,47 @@
 		},
 		// 顯示操作說明 (首次)
 		_showDirections: function () {
+			// 還沒顯示過才顯示
 			if (!this.dirShown) {
 				this.dirShown = true;
+				// 藉用 loading 的遮罩
 				loading();
+				// 藏掉 loading 圖示
 				$('#loading-position').hide();
+				// $dir: 說明區塊
+				// $cross: 關閉鈕
 				var $dir = $('<div></div>'),
 					$cross = $('<div>&times;</div>');
+				// 調整 style
 				$dir.css({
 					'position': 'fixed', 'left': '0', 'top': '0', 'right': '0', 'bottom': '0',
 					'margin': '20px', 'padding': '20px', 'border-radius': '15px', 'z-index': '999999',
-					'background-color': 'white', 'color': '#DD406F', 'font-size': '28px'
+					'background-color': 'white', 'color': '#DD406F', 'font-size': '28px',
+					'overflow-y': 'auto'
 				});
 				$cross.css({
 					'position': 'absolute', 'top': '5px', 'right': '15px',
 					'cursor': 'pointer'
 				});
+				// 加上內容
 				$dir.html([
 					'操作方式:<br/>',
 					'1. Ctrl + 滑鼠左鍵 Click 連續選取/取消選取左方或上方選項<br/>',
-					'2. 滑鼠左鍵 Click 選取/取消選取左方或上方選項並載入內容<br/><br/>',
+					'2. 滑鼠左鍵 Click 選取/取消選取左方或上方選項並載入內容<br/>',
+					'3. 點擊上方選項取消選取時, 會增加一個 "第二層區塊" 於左方選項上方, 方便將一度選取過又刪除的項目再重新選取而不必重新於左方選項中翻找<br/>',
+					'4. 第二層區塊中點擊 "全部清除" 可移除整個區塊, 點擊選項文字部份可重新選取選項, 點擊選項叉叉鈕可將該選項自第二層區塊中移除<br/><br/>',
 					'注意:<br/>',
-					'需在做任何選取操作之前啟用, 若已有做過選取操作, 請先滑鼠左鍵 Click 點擊任一選項<br/>',
-					'連續選取後需有一載入動作後下方分頁才可正確動作'
-				]);
+					'1. 需在做任何選取操作之前啟用, 若已有做過選取操作, 請先滑鼠左鍵 Click 點擊任一選項<br/>',
+					'2. 連續選取後需有一載入動作後下方分頁才可正確動作<br/>',
+					'3. 原始網頁內容均忠實呈現, 僅稍加修改操作流程、添加本操作說明及第二層選取區'
+				].join(''));
+				// 點關閉鈕時
 				$cross.on('click', function () {
+					// 移除 loading 及說明區塊
 					closeLoading();
 					$dir.remove();
 				});
+				// 將內容加到頁面上
 				$dir.append($cross);
 				$('body').append($dir);
 			}
